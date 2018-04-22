b948e9292de192c0610c99243389155b0f593d15
myusername <jnelsen788@gmail.com>
2018-04-22 12:17:33 -0400

    Implemented a scaffold and floating action button in the Manage Emotions view. I should implement this in the rest of the views now.

--- lib/pages/manage_emotions_view.dart
+++ lib/pages/manage_emotions_view.dart
@@ -18,91 +18,100 @@ class ManageEmotionsViewState extends State<ManageEmotionsView> {
 
   @override
   Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      body:
 
-    ///Top level container for the manage emotion view
-    return new Container(
-      color: Colors.green,
+        new Container(
+          color: Colors.green,
 
-      ///The row in the container containing the elements in the view
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
+          ///The row in the container containing the elements in the view
+          child: new Row(
+            children: <Widget>[
+              new Expanded(
 
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
 
-                  ///Title Info Bar
-                  new Container(
-                    decoration: new BoxDecoration(
-                      color: Colors.redAccent,
-                      border: new Border.all(color: Colors.black),
-                    ),
-                    child: new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-
-                        ///Emotion Column Title
-                        new Column(
+                      ///Title Info Bar
+                      new Container(
+                        decoration: new BoxDecoration(
+                          color: Colors.redAccent,
+                          border: new Border.all(color: Colors.black),
+                        ),
+                        child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            new Container(
-                              color: Colors.redAccent,
-                              padding: const EdgeInsets.all(10.0),
-                              child: new Text("Emotion",
-                                style: new TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0
-                                ),
-                              ),
-                            )
+
+                            ///Emotion Column Title
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  color: Colors.redAccent,
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: new Text("Emotion",
+                                    style: new TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+
+                            ///Tracking Column Title
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  color: Colors.redAccent,
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: new Text("Tracking",
+                                    style: new TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+
                           ],
                         ),
-
-                        ///Tracking Column Title
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              color: Colors.redAccent,
-                              padding: const EdgeInsets.all(10.0),
-                              child: new Text("Tracking",
-                                style: new TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-
-                      ],
-                    ),
-                  ),
-
-
-                  new Expanded (
-                      child:
-                      new ListView(
-                        children:
-                        _emotionWidgets.map((ManageEmotionListItem item) {
-                          return new Container(
-                            child: item,
-                          );
-                        }).toList(),
-
                       ),
-                  ),
+
+
+                      new Expanded (
+                          child:
+                          new ListView(
+                            children:
+                            _emotionWidgets.map((ManageEmotionListItem item) {
+                              return new Container(
+                                child: item,
+                              );
+                            }).toList(),
+
+                          ),
+                      ),
 
 
 
-                  ///The add emotion button
+                      ///The add emotion button
 
 
 
-                ],
+                    ],
+                  )
               )
-          )
-        ],
+            ],
+          ),
+        ),
+
+      floatingActionButton: new FloatingActionButton(
+        onPressed: addEmotion,
+        child: new Icon(Icons.add),
       ),
+
     );
 
   }

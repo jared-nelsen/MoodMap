b560f10b41fe37f629394bdb80705ece9ce64124
myusername <jnelsen788@gmail.com>
2018-05-05 15:41:13 -0400

    Finished making the text input box for the categories pretty.

--- lib/rating/categories_rate.dart
+++ lib/rating/categories_rate.dart
@@ -57,19 +57,51 @@ class RateCategoriesViewState extends State<RateCategoriesView> {
       child: new SimpleDialog(
         title: new Text("Add a category"),
         children: <Widget>[
-          new TextField(
-            autofocus: true,
-            maxLength: 20,
-            maxLengthEnforced: true,
-            onChanged: (String text) {toAdd = text;},
-          ),
-          new RaisedButton(
-              child: new Text("Add"),
-              onPressed: () {
-                setState(() {
-                  _emotions.add(new NavigableListItem(toAdd, _navigateToSpecifics));
-                  Navigator.pop(context, null);
-                });})
+          new Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: new TextField(
+                decoration: new InputDecoration(
+                    hintText: "Add a new category",
+                    border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(const Radius.circular(0.0)),
+                        borderSide: new BorderSide(color: Colors.black, width: 1.0)
+                    )
+                ),
+                autofocus: true,
+                maxLengthEnforced: true,
+                onChanged: (String text) {toAdd = text;},
+              ),),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    new FlatButton(
+                        child: new Text("Add", style: new TextStyle(color: Colors.green,),),
+                        onPressed: () {
+                          if(toAdd.isNotEmpty) {
+                            setState(() {
+                              _emotions.add(new NavigableListItem(toAdd, _navigateToSpecifics));
+                              Navigator.pop(context, null);
+                            });}
+                          }
+                    )],
+                ),
+              ),
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  new FlatButton(
+                      child: new Text("Cancel"),
+                      onPressed: () {
+                        setState(() {
+                          Navigator.pop(context, null);
+                        });})
+                ],
+              )
+            ],
+          )
         ],
       )
     );

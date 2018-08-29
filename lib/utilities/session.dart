2a13a25b52db9850603cfbb765d6e785b514bcf7
myusername <jnelsen788@gmail.com>
2018-08-28 22:33:26 -0400

    Filled out Session file with Create User and Login.

--- lib/utilities/session.dart
+++ lib/utilities/session.dart
@@ -1,16 +1,38 @@
 
+import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Session {
 
-  static FirebaseUser user;
+  static final FirebaseAuth authenticator = FirebaseAuth.instance;
 
-  static bool login(String username, String password) {
-
+  static bool login(String email, String password) {
+    _login(email, password).then((bool success) { return success; });
   }
 
-  static bool createAccount(String username, String password) {
+  static Future<bool> _login(String email, String password) async {
+    return await authenticator.signInWithEmailAndPassword(email: email, password: password)
+        .then((FirebaseUser user) { return true; })
+        .catchError((Error e) { return false; });
+  }
 
+  static bool createUser(String email, String password) {
+    _createUser(email, password).then((bool success) { return success; });
+  }
+
+  static Future<bool> _createUser(String email, String password) async {
+    return await authenticator.createUserWithEmailAndPassword(email: email, password: password)
+        .then((FirebaseUser user) { return true; })
+        .catchError((Error e) { return false; });
+  }
+
+
+  static bool userAlreadyExists(String email) {
+    _fetchEmailProviders(email).then((providers) { return providers.isNotEmpty; });
+  }
+
+  static Future<List<String>> _fetchEmailProviders(String email) async {
+    return await authenticator.fetchProvidersForEmail(email: email);
   }
 
 }

ab2dfe206908033da0890ab7a79190626571a1c1
myusername <jnelsen788@gmail.com>
2018-11-15 22:04:22 -0700

    Changed the way journal entry titles are formatted.

--- lib/components/journal_entry_list_item.dart
+++ lib/components/journal_entry_list_item.dart
@@ -30,12 +30,63 @@ class JournalEntryListItemState extends State<JournalEntryListItem> {
   Widget build(BuildContext context) {
 
     return new ListTile(
-      title: new Text(_journalEntry.getDate(), style: new TextStyle(fontSize: 16.0),),
+      title: new Text(_formatJournalDate(_journalEntry.getDate()), style: new TextStyle(fontSize: 16.0),),
       trailing: new FlatButton(
           onPressed: (){ _journalingContext.navigateToViewEntryView(_journalEntry); },
           child: new Icon(Icons.arrow_forward)),
     );
 
+  }
+
+  String _formatJournalDate(String journalDate) {
+
+    StringBuffer formatted = new StringBuffer();
+
+    var numeralsToMonths = {
+      "1": "January",
+      "2": "February",
+      "3": "March",
+      "4": "April",
+      "5": "May",
+      "6": "June",
+      "7": "July",
+      "8": "August",
+      "9": "September",
+      "10": "October",
+      "11": "November",
+      "12": "December",
+    };
+
+    List<String> parts = journalDate.split('/');
+
+
+    formatted.write(numeralsToMonths[parts.elementAt(0)]);
+    formatted.write(" ");
+    formatted.write(parts.elementAt(1));
+    int day = int.parse(parts.elementAt(1));
+    switch(day) {
+      case 1:
+      case 21:
+      case 31:
+        formatted.write("st");
+        break;
+      case 2:
+      case 22:
+        formatted.write("nd");
+        break;
+      case 3:
+      case 23:
+        formatted.write("rd");
+        break;
+      case 4:
+      case 24:
+        formatted.write("th");
+    }
+
+    formatted.write(", ");
+    formatted.write(parts.elementAt(2));
+
+    return formatted.toString();
   }
 
 
